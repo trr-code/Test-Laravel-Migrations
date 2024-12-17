d4e9f05eeccc9b0794c498d704d520edbcde6086
trr-code <154942624+trr-code@users.noreply.github.com>
2024-12-16 18:31:14 -0600

    Update 2021_11_09_082205_create_products_table.php

--- database/migrations/task4/2021_11_09_082205_create_products_table.php
+++ database/migrations/task4/2021_11_09_082205_create_products_table.php
@@ -13,11 +13,11 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        // TASK: Edit this file, so that deleting category would auto-delete its products
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
